2adabb25f805bd250c8fbab8adcb56ad7bdff4f1
Matheus Frej <matheusfrej@hotmail.com>
2023-12-31 15:05:17 -0300

    Passa regras de negócio para o caso de uso

--- src/http/controllers/users/send-reset-password-link.ts
+++ src/http/controllers/users/send-reset-password-link.ts
@@ -1,8 +1,5 @@
-import { resend } from "@/email/client";
-import { getParsedBody } from "@/email/parser";
 import { PreConditionalError } from "@/use-cases/errors/pre-conditional-error";
 import { makeSendPasswordLinkUseCase } from "@/use-cases/factories/make-send-password-link-use-case";
-import { generateChangePasswordToken } from "@/utils/generate-reset-password-token";
 import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 
@@ -22,24 +19,6 @@ export async function sendResetPasswordLink(
 
     await sendPasswordLinkUseCase.execute({ email });
 
-    const jwtWithEmail = generateChangePasswordToken(email);
-
-    const emailType: EmailType = {
-      type: "RESET_PASSWORD",
-      params: {
-        APP_URL: "www.google.com",
-        USER_EMAIL: email,
-      },
-    };
-
-    const html = getParsedBody(emailType);
-
-    await resend.emails.send({
-      from: "Suporte do Agendaê <suporte@agendae.fun>",
-      to: [email],
-      subject: "Recuperar senha",
-      html,
-    });
     return res.status(200).send();
   } catch (err) {
     if (err instanceof PreConditionalError) {
--- src/use-cases/send-password-link.ts
+++ src/use-cases/send-password-link.ts
@@ -1,5 +1,8 @@
 import { UsersRepositoryInterface } from "@/repositories/users-repository-interface";
 import { EmailNotFoundError } from "./errors/email-not-found";
+import { generateChangePasswordToken } from "@/utils/generate-reset-password-token";
+import { getParsedBody } from "@/email/parser";
+import { resend } from "@/email/client";
 
 interface SendPasswordLinkUseCaseRequest {
   email: string;
@@ -14,5 +17,24 @@ export class SendPasswordLinkUseCase {
     if (!user) {
       throw new EmailNotFoundError();
     }
+
+    const jwtWithEmail = generateChangePasswordToken(email);
+
+    const emailType: EmailType = {
+      type: "RESET_PASSWORD",
+      params: {
+        APP_URL: "www.google.com",
+        USER_EMAIL: email,
+      },
+    };
+
+    const html = getParsedBody(emailType);
+
+    await resend.emails.send({
+      from: "Suporte do Agendaê <suporte@agendae.fun>",
+      to: [email],
+      subject: "Recuperar senha",
+      html,
+    });
   }
 }
